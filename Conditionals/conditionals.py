1fa3314c5a7a4c4689e62420a117095fe7ad0e6f
Cristina <124594476+cristinasprogrammingadventure@users.noreply.github.com>
2024-05-24 13:01:53 +0200

    Conditionals in Python
    Introduction to conditionals in Python

---
+++ Conditionals/conditionals.py
@@ -0,0 +1,16 @@
+# imprimir el resultado con el bucle 'for'
+list = [5, 10, 15, 20]
+result = 0
+
+for number in list:
+    result += number
+print(result)
+
+
+##########################################
+
+# imprimir el resultado sin el bucle 'for'
+list2 = [5, 10, 15, 20]
+
+result2 = sum(list2)
+print(result2)
